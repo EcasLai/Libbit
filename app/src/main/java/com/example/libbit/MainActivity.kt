8e225116911cd4fc95c1252568737196ed2e342e
EcasLai <80811353+EcasLai@users.noreply.github.com>
2024-02-02 23:49:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/libbit/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.libbit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout_main, fragment)
+        fragmentTransaction.commit()
+    }
+}
