b7d9208ff0edacb4ea0028855c25924b8fe6775b
EcasLai <80811353+EcasLai@users.noreply.github.com>
2024-03-01 00:25:44 +0800

    Initial Commit with layout, activity, fragment,function

--- app/src/main/java/com/example/libbit/MainActivity.kt
+++ app/src/main/java/com/example/libbit/MainActivity.kt
@@ -3,11 +3,48 @@ package com.example.libbit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.libbit.model.Book
+import com.example.libbit.adapter.BookAdapter
+import com.example.libbit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var bookList: ArrayList<Book>
+    private lateinit var bookAdapter: BookAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //On start Home fragment
+        binding.bottomNavigationView.selectedItemId = R.id.bottom_nav_home
+
+        //Bottom Navigation
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.bottom_nav_book -> replaceFragment(BookFragment())
+                R.id.bottom_nav_saved ->  replaceFragment(SavedFragment())
+                R.id.bottom_nav_home ->  replaceFragment(HomeFragment())
+                R.id.bottom_nav_notification ->  replaceFragment(NotificationActivities())
+                R.id.bottom_nav_profile ->  replaceFragment(ProfileFragment())
+
+                else->{
+
+                }
+
+            }
+            true
+        }
+
+
+
+        replaceFragment(HomeFragment())
+
     }
 
     private fun replaceFragment(fragment : Fragment){
---
+++ app/src/main/java/com/example/libbit/ProfileFragment.kt
@@ -0,0 +1,37 @@
+package com.example.libbit
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.libbit.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.profileImg.setImageResource(R.drawable.book1)
+
+    }
+
+}
